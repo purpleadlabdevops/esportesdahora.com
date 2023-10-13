dd3c4d7a45beba44775da68f4df8ff57c21d6a38
Mykola <nickryz87@gmail.com>
2023-10-13 09:40:33 +0300

    changed links on v4

--- pages/v4.vue
+++ pages/v4.vue
@@ -9,7 +9,7 @@
           <p class="font-medium leading-tight my-3 text-lg">Lá você terá SINAIS enviados por mim todos os dias, e vai precisar <span class="text-accent-yellow">apenas copiar e colar</span>. Sem precisar saber nada sobre o mercado.</p>
           <p class="font-medium leading-tight my-3 text-lg">Todos os dias serão enviados de 10 a 15 sinais <span class="text-accent-yellow">GRATUITOS</span>.</p>
           <p class="font-medium leading-tight my-3 text-lg">Receba seu acesso usando o aplicativo <span class="text-accent-yellow">Telegram</span>. É só clicar no botão abaixo:</p>
-          <a href="https://plecombotionald.com/click" target="_blank" class="bg-accent-yellow flex items-center my-7 pulse-scale px-6 py-4 rounded-2xl text-black">
+          <a href="https://track.esportesdahora.com/click" target="_blank" class="bg-accent-yellow flex items-center my-7 pulse-scale px-6 py-4 rounded-2xl text-black">
             <div>
               <svg width="50" height="50" style="display: block"><path d="M25 2c12.703 0 23 10.297 23 23S37.703 48 25 48 2 37.703 2 25 12.297 2 25 2zm7.934 32.375c.423-1.298 2.405-14.234 2.65-16.783.074-.772-.17-1.285-.648-1.514-.578-.278-1.434-.139-2.427.219-1.362.491-18.774 7.884-19.78 8.312-.954.405-1.856.847-1.856 1.487 0 .45.267.703 1.003.966.766.273 2.695.858 3.834 1.172 1.097.303 2.346.04 3.046-.395.742-.461 9.305-6.191 9.92-6.693.614-.502 1.104.141.602.644-.502.502-6.38 6.207-7.155 6.997-.941.959-.273 1.953.358 2.351.721.454 5.906 3.932 6.687 4.49.781.558 1.573.811 2.298.811.725 0 1.107-.955 1.468-2.064z"></path></svg>
             </div>
